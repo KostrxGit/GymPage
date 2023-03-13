dda6f0e0e8a4645b0b82170e0297b7aef05f2c35
Konstanty <96338432+KostrxGit@users.noreply.github.com>
2023-03-13 09:03:44 +0100

    Update MobileNav

--- src/Components/Navbar/MobileNavLinks.tsx
+++ src/Components/Navbar/MobileNavLinks.tsx
@@ -1,16 +1,22 @@
-import LoginBtn from "./loginBtn";
+import { Link } from "react-router-dom";
 
 const MobileNavLinks = () => {
-    return (
-
-        <ul id="menu">
-            <a href="index.html"><li className='tekst'>Home</li></a>
-            <a href="#"><li className='tekst'>About</li></a>
-            <a href="#"><li className='tekst'>Contact</li></a>
-            <a href="#"> <li className='tekst'>Plans</li></a>
-            {/* <li><LoginBtn /></li> */}
-        </ul>
-    );
-}
+  return (
+    <ul id="menu">
+      <Link to="/">
+        <li className="tekst">Home</li>
+      </Link>
+      <Link to="/about">
+        <li className="tekst">About</li>
+      </Link>
+      <Link to="/contact">
+        <li className="tekst">Contact</li>
+      </Link>
+      <Link to="/plans">
+        <li className="tekst">Plans</li>
+      </Link>
+    </ul>
+  );
+};
 
 export default MobileNavLinks;
