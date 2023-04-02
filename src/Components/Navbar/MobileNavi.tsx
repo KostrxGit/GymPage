674c8ac68b396ccf03ead025162879d87e5571f7
Konstanty <96338432+KostrxGit@users.noreply.github.com>
2023-04-02 18:45:57 +0200

    Idk wtf is taht

--- src/Components/Navbar/MobileNavi.tsx
+++ src/Components/Navbar/MobileNavi.tsx
@@ -1,27 +1,22 @@
 import MoblieNavLinks from "./MobileNavLinks";
-import './navbar.css';
+import "./navbar.css";
 
 const MobileNav = () => {
+  return (
+    <nav role="navigation" className="navigation">
+      <div className="hamMargin">
+        <div id="menuToggle">
+          <input type="checkbox" />
 
+          <span></span>
+          <span></span>
+          <span></span>
 
-    return (
-        <nav role="navigation" className="navigation">
-            <div className="hamMargin">
-                <div id="menuToggle">
-
-                    <input type="checkbox" />
-
-
-                    <span></span>
-                    <span></span>
-                    <span></span>
-
-                    <MoblieNavLinks />
-                </div>
-            </div>
-        </nav>
-
-    );
-}
+          <MoblieNavLinks />
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default MobileNav;
--- src/Components/Navbar/MobileNaviBackup.tsx
+++ src/Components/Navbar/MobileNaviBackup.tsx
@@ -1,45 +1,40 @@
 import NavLinks from "./NavLinks";
-import logo from './logo.svg'
-import './navbar.css';
+import logo from "./logo.svg";
+import "./navbar.css";
 import { useState } from "react";
-
-
 
 // TODO: Zmienić całkowicie hamburger menu na jakieś inne
 
 const MobileNav = () => {
+  //to change burger clases
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
+  const [menu_class, setMenuClass] = useState("menu hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
 
-    //to change burger clases
-    const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
-    const [menu_class, setMenuClass] = useState("menu hidden");
-    const [isMenuClicked, setIsMenuClicked] = useState(false);
-
-    //toggle burger menu change
-    const updateMenu = () => {
-        if (!isMenuClicked) {
-            setBurgerClass("burger-bar clicked");
-            setMenuClass("menu visible");
-        }
-        else {
-            setBurgerClass("burger-bar unclicked");
-            setMenuClass("menu hidden");
-        }
+  //toggle burger menu change
+  const updateMenu = () => {
+    if (!isMenuClicked) {
+      setBurgerClass("burger-bar clicked");
+      setMenuClass("menu visible");
+    } else {
+      setBurgerClass("burger-bar unclicked");
+      setMenuClass("menu hidden");
     }
+  };
 
-
-    return (
-        <div>
-            <nav>
-                <div className="burger-menu" onClick={updateMenu}>
-                    <div className={burger_class} onClick={updateMenu}></div>
-                    <div className={burger_class} onClick={updateMenu}></div>
-                    <div className={burger_class} onClick={updateMenu}></div>
-                </div>
-                <NavLinks />
-            </nav>
-            <div className={menu_class}></div>
+  return (
+    <div>
+      <nav>
+        <div className="burger-menu" onClick={updateMenu}>
+          <div className={burger_class} onClick={updateMenu}></div>
+          <div className={burger_class} onClick={updateMenu}></div>
+          <div className={burger_class} onClick={updateMenu}></div>
         </div>
-    );
-}
+        <NavLinks />
+      </nav>
+      <div className={menu_class}></div>
+    </div>
+  );
+};
 
 export default MobileNav;
